5c02a88f5da0304a34447f3ffae15a02a3719bfc
Justin Johnson <jjustin634@gmail.com>
2020-12-19 19:41:23 -0600

    adding front end validation

--- client/src/components/user/user.js
+++ client/src/components/user/user.js
@@ -1,16 +1,16 @@
 import React, { Component } from "react";
 import classnames from "classnames";
 import axios from "axios";
-import { Button } from "@material-ui/core";
+import { Button, FormControl } from "@material-ui/core";
 import TextField from '@material-ui/core/TextField';
 import './user.css';
 
 
 class User extends Component {
-    constructor() {
-        super();
+    constructor(props) {
+        super(props);
         this.state = {
-            email: "",
+            name: "",
             phone_number: "",
             errors: {}
         };
@@ -20,23 +20,46 @@ class User extends Component {
         this.setState({ [e.target.id]: e.target.value });
     };
 
+    formValidation = () => {
+        let isValid = true;
+        const errors = {};
+
+        if (this.state.name.trim().length < 1) {
+            errors.userNameLength = "Name is required!"
+            isValid = false;
+        }
+        if (this.state.phone_number.trim().length != 10) {
+            errors.phoneNumberLength = "Number must be 10 digits!"
+            isValid = false;
+        }
+
+        this.setState({ errors });
+        return isValid;
+    }
+
     onSubmit = e => {
         e.preventDefault();
+
+        const isValid = this.formValidation();
+
         // create form as we are using the formidable package on backend
-        let formData = new FormData();
+        if (isValid) {
+            let formData = new FormData();
 
-        // add data from state to form
-        formData.append('name', this.state.name);
-        formData.append('phone_number', this.state.phone_number);
+            // add data from state to form
+            formData.append('name', this.state.name);
+            formData.append('phone_number', this.state.phone_number);
 
-        console.log('hello??')
-        console.log(this.state.name)
-        console.log(this.state.phone_number)
-        axios.post(`/api/v1/user`, formData)
-            .then(res => {
-                console.log('removed user from liked post');
-            })
+            axios.post(`/api/v1/user`, formData)
+                .then(res => {
+                    console.log('Name and number added');
+                })
 
+            // reset form
+            this.setState({ name: "", phone_number: "" });
+        } else {
+            console.log('Invalid Form');
+        }
     };
 
     render() {
@@ -44,44 +67,44 @@ class User extends Component {
         return (
             <form noValidate className="user-form" onSubmit={this.onSubmit}>
                 <div className="center-user">
-                    <h5 className='login-alert login-banners'>Name <i className="fas fa-sign-in-alt"></i></h5>
+                    <h5 className='login-alert login-banners'>Enter Your Name and Number<i className="fas fa-sign-in-alt"></i></h5>
                     <div className="name-div">
                         <TextField
+                            noValidate
                             onChange={this.onChange}
-                            error={errors.name}
                             value={this.state.name}
                             type="name"
                             id="name"
                             label="Name"
+                            variant="outlined"
+                            size="small"
                             InputProps={{ disableUnderline: true }}
-                            className={classnames("", {
-                                invalid: errors.name || errors.namenotfound
-                            })}
                         />
-                        <p className="red-text">
-                            {errors.name}
-                        </p>
+                        {errors.userNameLength &&
+                            <p style={{ color: "red" }}>{errors.userNameLength}</p>
+                        }
+                        <p></p>
                     </div>
                     <div className="phone-number-div">
                         <TextField
+                            noValidate
                             onChange={this.onChange}
-                            error={errors.phone_number}
                             value={this.state.phone_number}
                             type="phone_number"
                             id="phone_number"
                             label="Phone Number"
+                            variant="outlined"
+                            size="small"
                             InputProps={{ disableUnderline: true }}
-                            className={classnames("", {
-                                invalid: errors.phone_number || errors.phone_number
-                            })}
                         />
-                        <p className="red-text">
-                            {errors.phone_number}
-                        </p>
+                        {errors.phoneNumberLength &&
+                            <p style={{ color: "red" }}>{errors.phoneNumberLength}</p>
+                        }
+                        <p></p>
                     </div>
-                    <Button type="submit" variant="outlined" className="button-color">
+                    <Button type="submit" variant="contained" color="primary">
                         Continue
-                </Button>
+                    </Button>
                 </div>
             </form>
         );
