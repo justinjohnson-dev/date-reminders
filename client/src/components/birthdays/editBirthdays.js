81ecf6e9d2f84778141f4905533ea8ca40fb139e
Justin Johnson <jjustin634@gmail.com>
2021-02-16 21:33:29 -0600

    adding editable table, filling table with mongo data

--- client/src/components/birthdays/editBirthdays.js
+++ client/src/components/birthdays/editBirthdays.js
@@ -1,38 +1,71 @@
 import React, { useState, useEffect, useRef } from 'react';
 import MaterialTable from "material-table";
+import axios from "axios";
 
 
 
 export default function Editable() {
     const { useState } = React;
-
+    const [userName, setUserName] = React.useState("");
+    const [userPhone, setUserPhone] = React.useState("");
+    const [data, setData] = useState([]);
     const [columns, setColumns] = useState([
-        { title: 'Name', field: 'name' },
-        { title: 'Surname', field: 'surname', initialEditValue: 'initial edit value' },
-        { title: 'Birth Year', field: 'birthYear', type: 'numeric' },
-        {
-            title: 'Birth Place',
-            field: 'birthCity',
-            lookup: { 34: 'İstanbul', 63: 'Şanlıurfa' },
-        },
+        { title: 'Name', field: 'birthdayName' },
+        { title: 'Birthday', field: 'birthdayDate', type: 'date' },
     ]);
 
-    const [data, setData] = useState([
-        { name: 'Mehmet', surname: 'Baran', birthYear: 1987, birthCity: 63 },
-        { name: 'Zerya Betül', surname: 'Baran', birthYear: 2017, birthCity: 34 },
-    ]);
+    useEffect(() => {
+        let userInformation = localStorage.getItem('user');
+        let userName = userInformation.substr(0, userInformation.indexOf(',')).trim();
+        let userPhone = userInformation.substr(userInformation.indexOf(','), userInformation.length).replace(',', '').trim();
+        setUserName(userName);
+        setUserPhone(userPhone);
+        axios.get(`/api/v1/birthdays/${userPhone}`)
+            .then(response => setData(response.data.birthdays));
+    }, []);
+
+    const uploadToMongo = () => {
+        console.log("upload to mongo")
+        console.log(data)
+        console.log(data.birthdayName)
+        console.log(data.birthdayDate)
+
+        let userInfo = {
+            birthdayName: data.birthdayName,
+            birthdayDate: data.birthdayName
+        }
+        if (data != undefined) {
+            axios.post(`/api/v1/userBirthdays`, userInfo)
+                .then(res => {
+                    console.log("userInfo")
+                    console.log(userInfo)
+                })
+        } else {
+            console.log('Sending no data - no birthdays were added')
+        }
+    }
 
     return (
         <MaterialTable
-            title="Editable Preview"
+            title="Birthdays"
             columns={columns}
             data={data}
+            style={{ width: "75%", margin: "0px auto", marginTop: "5%" }}
             editable={{
                 onRowAdd: newData =>
                     new Promise((resolve, reject) => {
                         setTimeout(() => {
-                            setData([...data, newData]);
-
+                            if (data == undefined) {
+                                setData([newData]);
+                                console.log('inside the function')
+                                console.log(data)
+                                uploadToMongo();
+                            } else {
+                                setData([...data, newData]);
+                                console.log('inside the else function')
+                                console.log(data)
+                                uploadToMongo();
+                            }
                             resolve();
                         }, 1000)
                     }),
@@ -43,7 +76,7 @@ export default function Editable() {
                             const index = oldData.tableData.id;
                             dataUpdate[index] = newData;
                             setData([...dataUpdate]);
-
+                            uploadToMongo();
                             resolve();
                         }, 1000)
                     }),
@@ -54,7 +87,7 @@ export default function Editable() {
                             const index = oldData.tableData.id;
                             dataDelete.splice(index, 1);
                             setData([...dataDelete]);
-
+                            uploadToMongo();
                             resolve()
                         }, 1000)
                     }),
