678365a9b6601ff2a813b50e6b9dc693f0208190
Justin Johnson <jjustin634@gmail.com>
2021-02-23 20:29:16 -0600

    adding icons to be able to customize for edible table

--- client/src/components/birthdays/birthday.js
+++ client/src/components/birthdays/birthday.js
@@ -1,9 +1,24 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, forwardRef } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import { Button } from "@material-ui/core";
 import MaterialTable from "material-table";
 import Paper from '@material-ui/core/Paper';
 import axios from "axios";
+import AddBox from '@material-ui/icons/AddBox';
+import ArrowDownward from '@material-ui/icons/ArrowDownward';
+import Check from '@material-ui/icons/Check';
+import ChevronLeft from '@material-ui/icons/ChevronLeft';
+import ChevronRight from '@material-ui/icons/ChevronRight';
+import Clear from '@material-ui/icons/Clear';
+import DeleteOutline from '@material-ui/icons/DeleteOutline';
+import Edit from '@material-ui/icons/Edit';
+import FilterList from '@material-ui/icons/FilterList';
+import FirstPage from '@material-ui/icons/FirstPage';
+import LastPage from '@material-ui/icons/LastPage';
+import Remove from '@material-ui/icons/Remove';
+import SaveAlt from '@material-ui/icons/SaveAlt';
+import Search from '@material-ui/icons/Search';
+import ViewColumn from '@material-ui/icons/ViewColumn';
 import './birthday.css';
 
 
@@ -98,6 +113,27 @@ export default function Editable() {
         )
     }
 
+    const tableIcons = {
+        Add: forwardRef((props, ref) => <AddBox {...props} ref={ref} />),
+        Check: forwardRef((props, ref) => <Check {...props} style={{ color: 'green' }} ref={ref} />),
+        Clear: forwardRef((props, ref) => <Clear {...props} style={{ color: 'red' }} ref={ref} />),
+        Delete: forwardRef((props, ref) => <DeleteOutline {...props} ref={ref} />),
+        DetailPanel: forwardRef((props, ref) => <ChevronRight {...props} ref={ref} />),
+        Edit: forwardRef((props, ref) => <Edit {...props} ref={ref} />),
+        Export: forwardRef((props, ref) => <SaveAlt {...props} ref={ref} />),
+        Filter: forwardRef((props, ref) => <FilterList {...props} ref={ref} />),
+        FirstPage: forwardRef((props, ref) => <FirstPage {...props} ref={ref} />),
+        LastPage: forwardRef((props, ref) => <LastPage {...props} ref={ref} />),
+        NextPage: forwardRef((props, ref) => <ChevronRight {...props} ref={ref} />),
+        PreviousPage: forwardRef((props, ref) => <ChevronLeft {...props} ref={ref} />),
+        ResetSearch: forwardRef((props, ref) => <Clear {...props} ref={ref} />),
+        Search: forwardRef((props, ref) => <Search {...props} ref={ref} />),
+        SortArrow: forwardRef((props, ref) => <ArrowDownward {...props} ref={ref} />),
+        ThirdStateCheck: forwardRef((props, ref) => <Remove {...props} ref={ref} />),
+        ViewColumn: forwardRef((props, ref) => <ViewColumn {...props} ref={ref} />)
+    };
+
+
     return (
         <div className={classes.root} >
             <Paper style={{ backgroundColor: "#eee" }}>
@@ -111,15 +147,8 @@ export default function Editable() {
                 title={fetchTitle()}
                 columns={columns}
                 data={data}
+                icons={tableIcons}
                 style={{ marginTop: "5%" }}
-                // icons={{
-                //     Add: props => (
-                //         <div ref={myTextIcon} className={classes.addButton}>
-                //             <i className="fa fa-plus" />
-                //             Add new Region
-                //         </div>
-                //     ),
-                // }}
                 editable={{
                     onRowAdd: newData =>
                         new Promise((resolve, reject) => {
