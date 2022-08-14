7b68a925dac9746cabece0b4c39860b3db8c2199
Justin Johnson <jjustin634@gmail.com>
2022-08-14 12:53:22 -0500

    adding layout with appbar and footer

--- components/appbar/index.tsx
+++ components/appbar/index.tsx
@@ -1,64 +1,115 @@
-import React from 'react';
+import * as React from 'react';
 import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Divider from '@mui/material/Divider';
+import Drawer from '@mui/material/Drawer';
+import IconButton from '@mui/material/IconButton';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemText from '@mui/material/ListItemText';
+import MenuIcon from '@mui/icons-material/Menu';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
-import CssBaseline from '@mui/material/CssBaseline';
-import useScrollTrigger from '@mui/material/useScrollTrigger';
-import Box from '@mui/material/Box';
-import Container from '@mui/material/Container';
-import Slide from '@mui/material/Slide';
+import { styled } from '@mui/material/styles';
+import Button, { ButtonProps } from '@mui/material/Button';
+import { teal } from '@mui/material/colors';
+
+const ColorButton = styled(Button)<ButtonProps>(({ theme }) => ({
+  color: theme.palette.getContrastText(teal[500]),
+  backgroundColor: teal[500],
+  '&:hover': {
+    backgroundColor: teal[700],
+  },
+}));
 
 interface Props {
-  /**
-   * Injected by the documentation to work in an iframe.
-   * You won't need it on your project.
-   */
   window?: () => Window;
-  children: React.ReactElement;
 }
 
-function HideOnScroll(props: Props) {
-  const { children, window } = props;
-  // Note that you normally won't need to set the window ref as useScrollTrigger
-  // will default to window.
-  // This is only being set here because the demo is in an iframe.
-  const trigger = useScrollTrigger({
-    target: window ? window() : undefined,
-  });
-
-  return (
-    <Slide appear={false} direction='down' in={!trigger}>
-      {children}
-    </Slide>
-  );
-}
+const drawerWidth = 240;
+const navItems = ['Home', 'About', 'Contact'];
 
 export default function ApplicationBar(props: Props) {
+  const { window } = props;
+  const [mobileOpen, setMobileOpen] = React.useState(false);
+
+  const handleDrawerToggle = () => {
+    setMobileOpen(!mobileOpen);
+  };
+
+  const drawer = (
+    <Box onClick={handleDrawerToggle} sx={{ textAlign: 'center' }}>
+      <Typography variant='h6' sx={{ my: 2 }}>
+        MUI
+      </Typography>
+      <Divider />
+      <List>
+        {navItems.map((item) => (
+          <ListItem key={item} disablePadding>
+            <ListItemButton sx={{ textAlign: 'center' }}>
+              <ListItemText primary={item} />
+            </ListItemButton>
+          </ListItem>
+        ))}
+        <ColorButton variant='contained'>Sign Out</ColorButton>
+      </List>
+    </Box>
+  );
+
+  const container =
+    window !== undefined ? () => window().document.body : undefined;
+
   return (
-    <>
-      <CssBaseline />
-      <HideOnScroll {...props}>
-        <AppBar color='default'>
-          <Toolbar>
-            <Typography variant='h6' component='div'>
-              The Birthday Reminder
-            </Typography>
-          </Toolbar>
-        </AppBar>
-      </HideOnScroll>
-      <Toolbar />
-      <Container>
-        <Box sx={{ my: 2 }}>
-          {[...new Array(12)]
-            .map(
-              () => `Cras mattis consectetur purus sit amet fermentum.
-Cras justo odio, dapibus ac facilisis in, egestas eget quam.
-Morbi leo risus, porta ac consectetur ac, vestibulum at eros.
-Praesent commodo cursus magna, vel scelerisque nisl consectetur et.`,
-            )
-            .join('\n')}
-        </Box>
-      </Container>
-    </>
+    <Box sx={{ display: 'flex', height: '55px' }}>
+      <AppBar component='nav'>
+        <Toolbar>
+          <IconButton
+            color='inherit'
+            aria-label='open drawer'
+            edge='start'
+            onClick={handleDrawerToggle}
+            sx={{ mr: 2, display: { sm: 'none' } }}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography
+            variant='h6'
+            component='div'
+            sx={{ flexGrow: 1, display: { xs: 'none', sm: 'block' } }}
+          >
+            The Birthday Reminder
+          </Typography>
+          <Box sx={{ display: { xs: 'none', sm: 'block' } }}>
+            {navItems.map((item) => (
+              <Button key={item} sx={{ color: '#fff' }}>
+                {item}
+              </Button>
+            ))}
+            <ColorButton variant='contained'>Sign Out</ColorButton>
+          </Box>
+        </Toolbar>
+      </AppBar>
+      <Box component='nav'>
+        <Drawer
+          container={container}
+          variant='temporary'
+          open={mobileOpen}
+          onClose={handleDrawerToggle}
+          ModalProps={{
+            keepMounted: true, // Better open performance on mobile.
+          }}
+          sx={{
+            display: { xs: 'block', sm: 'none' },
+            '& .MuiDrawer-paper': {
+              boxSizing: 'border-box',
+              width: drawerWidth,
+            },
+          }}
+        >
+          {drawer}
+        </Drawer>
+      </Box>
+    </Box>
   );
 }
--- components/footer/footer.tsx
+++ components/footer/footer.tsx
@@ -4,9 +4,52 @@ interface FooterProps {}
 
 const Footer: FC<FooterProps> = ({}) => {
   return (
-    <>
-      <h1>Footer</h1>
-    </>
+    <footer
+      style={{
+        backgroundColor: '#eee',
+        padding: '5%',
+        position: 'absolute',
+        bottom: 0,
+        width: '100%',
+        height: '2.5rem',
+      }}
+    >
+      <div className='container'>
+        {/* <ul className='list-unstyled list-inline text-center'>
+          <li className='list-inline-item'>
+            <a
+              className='btn-floating btn-li mx-1'
+              target='_blank'
+              href='https://www.linkedin.com/in/justin-johnson-413a93169/'
+            >
+              <i className='fab fa-linkedin'> </i>
+            </a>
+          </li>
+          <li className='list-inline-item'>
+            <a
+              className='btn-floating btn-envelope mx-1'
+              href='mailto:jjustin634@gmail.com'
+            >
+              <i className='far fa-envelope'></i>
+            </a>
+          </li>
+          <li className='list-inline-item'>
+            <a
+              className='btn-floating mx-1'
+              target='_blank'
+              href='https://github.com/justinjohnson-dev'
+            >
+              <i className='fab fa-github'> </i>
+            </a>
+          </li>
+        </ul> */}
+      </div>
+      <div className='footer-copyright text-center py-3'>
+        © 2022 Copyright - Stay Connected!
+        {/* <a href="https://mdbootstrap.com/bootstrap-tutorial/"> thebirthdayreminder.com</a>
+         */}
+      </div>
+    </footer>
   );
 };
 
