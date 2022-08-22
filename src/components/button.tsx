e233cac81e03887c0ac5abf2afbf09c6fae85f93
Justin Johnson <jjustin634@gmail.com>
2022-08-22 18:48:50 -0500

    adding tailwindcss styles (#9)

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -1,15 +1,4 @@
 import React, { FC } from 'react';
-import { styled } from '@mui/material/styles';
-import Button, { ButtonProps } from '@mui/material/Button';
-import { teal } from '@mui/material/colors';
-
-const ColorButton = styled(Button)<ButtonProps>(({ theme }) => ({
-  color: theme.palette.getContrastText(teal[500]),
-  backgroundColor: teal[500],
-  '&:hover': {
-    backgroundColor: teal[700],
-  },
-}));
 
 interface StyledButtonProps {
   label: string;
@@ -18,9 +7,12 @@ interface StyledButtonProps {
 
 const TealButton: FC<StyledButtonProps> = ({ label, ...props }) => {
   return (
-    <ColorButton variant='contained' {...props}>
+    <button
+      className='bg-teal-700 hover:bg-teal-600 text-white py-2 px-4 rounded'
+      {...props}
+    >
       {label}
-    </ColorButton>
+    </button>
   );
 };
 
